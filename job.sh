b6c75f3540a8f534209a1ec974f29ee10ed24336
Nikhil Verma <nikhilv0987@gmail.com>
2026-02-27 02:22:14 +0000

    updated job.sh

--- job.sh
+++ job.sh
@@ -7,15 +7,6 @@
 #SBATCH -t 1-02:30:00         
 #SBATCH --gres=gpu:rtxa6000:1 
 
+source Practicum/bin/activate
 
-module load python/3.12.12
-
-python -m venv my_pipeline_env
-
-source my_pipeline_env/bin/activate
-
-pip install --upgrade pip
-
-pip install -r requirements.txt
-
-python main.py --wandb_run_name="NLLB-First-train" --save_steps=17000 --logging_steps=500 --num_train_epochs=2 --learning_rate=2e-4 --batch_size=32 --eval_batch_size=16
+uv run main.py --wandb_run_name="NLLB-First-train" --save_steps=17000 --logging_steps=500 --num_train_epochs=2 --learning_rate=2e-4 --batch_size=32 --eval_batch_size=16
